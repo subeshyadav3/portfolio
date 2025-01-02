dc28c3203e0e6dae0490ac1b4c7a0ec803a8543c
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-01-02 20:01:41 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // Deploy at root, not in a subfolder
+  base: '/', // Deploy at root, not in a subfolder
   plugins: [react()],
 });
