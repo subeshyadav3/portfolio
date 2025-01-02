870abb4df86df635913174f164a23137a469549b
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-01-02 20:16:55 +0545

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: [
+    "./index.html", 
       './src/**/*.{html,js,ts,jsx,tsx}', // Adjust paths based on your project structure
   ],
   theme: {
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // Deploy at root, not in a subfolder
   plugins: [react()],
 });
